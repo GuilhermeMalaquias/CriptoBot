556a84335ce4d2923acc3c33362ca1558d4b7897
Guilherme Malaquias <silvaam.guilherme@gmail.com>
2020-10-14 16:31:51 -0300

    Correcao da funcao mostrar_moeda_menu

--- CriptoBot/menu.py
+++ CriptoBot/menu.py
@@ -1,18 +1,17 @@
 class Menu:
     def __init__(self):
-        self.divisorMenu = '=' * 40
+        self.divMenu = '=' * 40
         self.infoBot = 'CriptoBot / Versao: 1.1.1'
         self.desenvolvedor = 'Guilherme Malaquias'
 
-    def inicial_menu(self):
-        return f'Seja Bem Vindo!\nDesenvolvedor: {self.desenvolvedor}\n{self.infoBot}'
+    def inicial_menu(self) -> str:
+        return f'{self.divMenu}\n1 - Consultar Moeda\n2 - Consultar Moeda Por Intervalo\n0 - Para sair\n{self.divMenu}'
 
-    def escolha_menu(self) -> str:
-        return '1 - Consultar Moeda\n2 - Consultar Moeda Por Intervalo\n0 - Para sair'
+    def mostra_moedas_menu(self) -> str:
+        return f'{self.divMenu}\n1 - BTC-BRL (Bitcoin)\n2 - LTC-BRL (Litecoin)\n3 - ETH-BRL (Ethereum)\n4 - XRP-BRL (Ripple)\n{self.divMenu}'
 
 
 if __name__ == "__main__":
     c = Menu()
     print(c.inicial_menu())
-    print(c.divisorMenu)
-    print(c.escolha_menu())
+    print(c.divMenu)
