ec594b2e302d6d61e3d720e091f0b6a227806fed
Guilherme Malaquias <silvaam.guilherme@gmail.com>
2020-10-17 15:32:14 -0300

    Reduzindo "If`s" usando dicionario e expressao lambda.

--- CriptoBot/main.py
+++ CriptoBot/main.py
@@ -7,22 +7,19 @@ menu = Menu()
 tipo_moeda = Moedas()
 requisicao = Request()
 
-sair = 0
 while True:
     print(menu.inicial_menu())
-    escolha_opcao = int(input('Escolha:'))
-    if escolha_opcao == sair:
-        print('Saindo...')
+    escolha = int(input('Escolha: '))
+    if escolha == 0:
+        print('\033[1;33mSaindo...\033[m')
         sleep(2)
         print(f'Desenvolvedor: {menu.desenvolvedor}\n{menu.infoBot}')
         break
-    elif escolha_opcao > 2 or escolha_opcao < 0:
-        print('\033[1;31mOpcao Invalida\033[m')
-    elif escolha_opcao == 1:
-        print(menu.mostra_moedas_menu())
+    try:
+        print(menu.escolha_menu(escolha))
         escolha_moeda = int(input('Escolha:'))
         requisicao.req(tipo_moeda.escolhas_moeda(escolha_moeda))
         print(requisicao.filter_req())
         break
-    elif escolha_opcao == 2:
-        print('\033[1;31mOpcao Indisponivel\033[m')
+    except KeyError:
+        print('\033[1;31mOpcao Invalida\033[m')
--- CriptoBot/menu.py
+++ CriptoBot/menu.py
@@ -3,15 +3,25 @@ class Menu:
         self.divMenu = '=' * 40
         self.infoBot = 'CriptoBot / Versao: 1.1.1'
         self.desenvolvedor = 'Guilherme Malaquias'
+        self.escolha = 1
 
     def inicial_menu(self) -> str:
         return f'{self.divMenu}\n1 - Consultar Moeda\n2 - Consultar Moeda Por Intervalo\n0 - Para sair\n{self.divMenu}'
 
     def mostra_moedas_menu(self) -> str:
-        return f'{self.divMenu}\n1 - BTC-BRL (Bitcoin)\n2 - LTC-BRL (Litecoin)\n3 - ETH-BRL (Ethereum)\n4 - XRP-BRL (Ripple)\n{self.divMenu}'
+        return f'{self.divMenu}\n1 - BTC-BRL (Bitcoin)\n2 - LTC-BRL (Litecoin)\n3 - ETH-BRL (Ethereum)\n4 - XRP-BRL (' \
+               f'Ripple)\n{self.divMenu}'
+
+    def escolha_menu(self, option: int):
+        self.option = option
+        self.temp = ''
+        escolha_dic = {
+            1: lambda temp: self.mostra_moedas_menu(),
+            2: lambda temp: '\033[1;31mOpcao Indisponivel\033[m',
+        }
+        return escolha_dic[self.option](self.temp)
 
 
 if __name__ == "__main__":
     c = Menu()
-    print(c.inicial_menu())
-    print(c.divMenu)
+    print(c.escolha_menu(2))
