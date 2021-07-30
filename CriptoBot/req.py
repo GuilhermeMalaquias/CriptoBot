42cb287e68d288f0f4fa629b972fb10185908ecb
vitorcardmore <72475294+vitorcardmore@users.noreply.github.com>
2021-07-30 15:24:41 -0300

    alterando o formato da data

--- CriptoBot/req.py
+++ CriptoBot/req.py
@@ -1,7 +1,8 @@
 from menu import Menu
 from datetime import datetime
-import json
 import requests
+from pytz import timezone as tmz
+
 
 menu = Menu()
 
@@ -33,19 +34,25 @@ class Request:
     def filter_req(self):
         self.name = self.context['name']
         self.atual = float(self.context['market_data']['current_price']['brl'])
-        # self.data = self.context['market_data']['ath_date']['brl']
-        # self.data = datetime.fromtimestamp(int(self.data)).strftime('%d/%m/%Y')
-        print(f'{menu.divMenu}\nNome: {self.name}\nValor: R$ {self.atual:.2f}\n{menu.divMenu}'.replace(
+        self.data = self.context['last_updated']
+        self.data_date, self.data_horas = self.data.split("T")
+        self.data_date = '{}/{}/{}'.format(self.data_date.split('-')[2], self.data_date.split('-')[1], self.data_date.split('-')[0])
+        print(f'{menu.divMenu}\nData: {self.data_date}\nNome: {self.name}\nValor: R$ {self.atual:.2f}\n{menu.divMenu}'.replace(
             '.',
             ','))
-
     def filter_req_intervalo(self):
+
+        
+
         print(menu.divMenu)
         for i in self.context_intervalo['prices']:
+
+            i[0] = datetime.fromtimestamp((i[0]/1000), tz = tmz('America/Sao_Paulo'))
+            i[0] = i[0].strftime('%d/%m/%Y')
             
-            print('Nome: {}\nData: {}\nValor: {}\n{}'.format(self.moeda.capitalize(), *i,menu.divMenu))
+            print('Nome: {}\nData: {}\nValor: R$ {:.2f}\n{}'.format(self.moeda.capitalize(), *i,menu.divMenu).replace('.', ','))
             
 
 
-              
+            
 
